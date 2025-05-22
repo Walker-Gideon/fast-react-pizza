0dd3645c5a08bfb33ed291765d229a05c248ab36
Gideon Awortwe Walker <gideonawortwe3@gmail.com>
2025-05-22 16:23:41 +0000

    Styly adjustment in the home section

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 function CartOverview() {
   return (
-    <div className="flex items-center justify-between bg-stone-800 text-stone-200">
+    <div className="flex items-center justify-between bg-stone-800 text-stone-200 uppercase">
       <p className="text-stone-300">
         <span>23 pizzas</span>
         <span>$23.45</span>
