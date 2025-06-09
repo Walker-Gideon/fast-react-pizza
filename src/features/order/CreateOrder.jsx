3ace42d3be41931ec6fc246b7c3acd966de63f03
Gideon Awortwe Walker <gideonawortwe3@gmail.com>
2025-06-09 22:18:41 +0000

    Reuse tailwind style

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -51,13 +51,13 @@ function CreateOrder() {
       <Form method="POST">
         <div>
           <label>First Name</label>
-          <input type="text" name="customer" required />
+          <input type="text" className="input" name="customer" required />
         </div>
 
         <div>
           <label>Phone number</label>
           <div>
-            <input type="tel" name="phone" required />
+            <input type="tel" className="input" name="phone" required />
           </div>
           {formErrors?.phone && <p>{formErrors.phone}</p>}
         </div>
@@ -65,12 +65,7 @@ function CreateOrder() {
         <div>
           <label>Address</label>
           <div>
-            <input
-              type="text"
-              className="w-full rounded-full border border-stone-200 px-4 py-2 transition-all duration-300 placeholder:text-stone-400 focus:ring focus:ring-yellow-400 focus:outline-none md:px-6 md:py-3"
-              name="address"
-              required
-            />
+            <input type="text" className="input" name="address" required />
           </div>
         </div>
 
