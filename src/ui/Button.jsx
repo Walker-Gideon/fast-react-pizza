56f20f3384f80e132bec8e18ce0629c9eb219ca4
Walker-Gideon <gideonawortwe3@gmail.com>
2025-07-15 01:21:00 +0000

    Created the LinkButton component

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -4,7 +4,7 @@ export default function Button({ children, disabled }) {
   return (
     <button
       disabled={disabled}
-      className="inline-block rounded-full bg-yellow-400 px-4 py-3 font-semibold tracking-wide text-stone-800 uppercase transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:ring focus:ring-yellow-300 focus:ring-offset-2 focus:outline-none disabled:cursor-not-allowed"
+      className="inline-block rounded-full bg-yellow-400 px-4 py-3 font-semibold tracking-wide text-stone-800 uppercase transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:ring focus:ring-yellow-300 focus:ring-offset-2 focus:outline-none disabled:cursor-not-allowed sm:px-6 sm:py-4"
     >
       {children}
     </button>
