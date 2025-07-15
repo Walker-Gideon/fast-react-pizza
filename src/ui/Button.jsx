969f8db32f179046535f8086511c462e59a07182
Walker-Gideon <gideonawortwe3@gmail.com>
2025-07-15 01:27:06 +0000

    Update the Button component with a Link

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,11 +1,19 @@
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
-export default function Button({ children, disabled }) {
+export default function Button({ children, disabled, to }) {
+  const className =
+    "inline-block rounded-full bg-yellow-400 px-4 py-3 font-semibold tracking-wide text-stone-800 uppercase transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:ring focus:ring-yellow-300 focus:ring-offset-2 focus:outline-none disabled:cursor-not-allowed sm:px-6 sm:py-4";
+
+  if (to)
+    return (
+      <Link to={to} className={className}>
+        {children}
+      </Link>
+    );
+
   return (
-    <button
-      disabled={disabled}
-      className="inline-block rounded-full bg-yellow-400 px-4 py-3 font-semibold tracking-wide text-stone-800 uppercase transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:ring focus:ring-yellow-300 focus:ring-offset-2 focus:outline-none disabled:cursor-not-allowed sm:px-6 sm:py-4"
-    >
+    <button disabled={disabled} className={className}>
       {children}
     </button>
   );
@@ -14,4 +22,5 @@ export default function Button({ children, disabled }) {
 Button.propTypes = {
   children: PropTypes.string,
   disabled: PropTypes.func,
+  to: PropTypes.string,
 };
